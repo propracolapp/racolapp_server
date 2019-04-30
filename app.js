460657146d9181fbeb71da52f297e59ff2f21616
Steeve Randriamampianina <st2diio@protonmail.ch>
2019-04-30 15:26:00 +0200

    mount server

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import router from './controllers/index';
+import api from './controllers/index';
+import cors from 'cors';
 
-const app = express();
-dotenv.config();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
- app.get('/hello', function (req, res) {
-     res.send('Hello World!')
- })
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
-app.listen(process.env.APP_PORT, () => {
-    console.log(`Server start on port: ${process.env.APP_PORT}`)
-})
+const start = async() =>{
+    try {
+        const app = express();
+        dotenv.config();
+        
+        app.use(bodyParser.json());
+        app.use(cors());
+
+        app.use(bodyParser.urlencoded({
+            extended: false
+        }));
+         app.get('/hello', function (req, res) {
+             res.send('Hello World!')
+         })
+        app.get('/', function (req, res) {
+            res.send('Hello World!')
+        })
+        app.listen(process.env.APP_PORT, () => {
+            console.log(`Server start on port: ${process.env.APP_PORT}`)
+        })
+        
+    } catch(err) {
+        console.log(`Error server doesn't reply`);
+    }
+}
+
+start();
---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import users from './users';
+import map from './map';
+import events from './events';
+
+const api = Router();
+
+api.use("/", (res, req) => {
+    res.json({
+        name: "racolapp",
+        status: "running"
+    })
+})
+
+api.use("/users", users)
+
+api.use("/events", events)
+
+api.use("/map", map)
+
+export default api;
