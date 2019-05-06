4715d337320570d26d44bd21bd71ea87c5844924
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-05-06 11:56:29 +0200

    add models

--- engine/core.js
+++ engine/core.js
@@ -1,15 +1,9 @@
-export default class Core{
-    constructor({
-        host,
-        port,
-        username,
-        database,
-        password
-    }){
-        this.host = host;
-        this.port = port;
-        this.username = username;
-        this.database = database;
-        this.password = password;
-    }
+export default class Core {
+	constructor({ host, port, username, database, password }) {
+		this.host = host;
+		this.port = port;
+		this.username = username;
+		this.database = database;
+		this.password = password;
+	}
 }
