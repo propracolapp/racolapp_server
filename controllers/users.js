70dca8b05d17fa5b7cdfe1835618f5b0ccba79d5
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-05-07 13:23:58 +0200

    add crud users

--- controllers/users.js
+++ controllers/users.js
@@ -4,15 +4,21 @@ import Users from "../models/Users";
 const api = Router();
 
 api.get("/", async (req, res) => {
-	const data = await Users.findAll();
-	console.log(data);
-	res.json({
-		data
-	});
+	const data = await Users.findAll()
+		.then(data => {
+			console.log(data);
+			res.json({
+				data
+			});
+		})
+		.catch(err => {
+			res.status(500);
+			res.json({ error: err.message });
+		});
 });
 
 api.post("/", (req, res) => {
-	Users.save({
+	Users.create({
 		ID: req.body.id,
 		pseudo: req.body.pseudo,
 		mail: req.body.mail,
@@ -27,8 +33,58 @@ api.post("/", (req, res) => {
 		})
 		.catch(function(error) {
 			res.status(500);
-			res.json({ error: error, stackError: error.stack });
+			res.json({ error: error.message });
 		});
 });
+// get user by id
+api.get("/:id", async (req, res) => {
+	const data = await Users.findByPk(req.params.id)
+		.then(data => {
+			res.status(200);
+			res.json({
+				data
+			});
+		})
+		.catch(err => {
+			res.status(500);
+			res.json({
+				err: err.message
+			});
+		});
+});
+// modify user by id
+api.put("/:id", (req, res, next) => {
+	Users.update(
+		{
+			pseudo: req.body.pseudo,
+			mail: req.body.mail,
+			birthday: req.body.birthday,
+			active: req.body.active,
+			premium: req.body.premium,
+			img_profile: req.body.img_profile
+		},
+		{ where: { ID: req.params.id }, returning: true, plain: true }
+	)
+		.then(function(data) {
+			res.status(200);
+			res.json(data.get({ plain: true }));
+		})
+		.catch(function(error) {
+			res.status(500);
+			res.json({ error: error.message });
+		});
+});
+// delete user by id
 
+api.delete("/:id", (req, res, next) => {
+	Users.destroy({
+		where: { id: id }
+	}).then(data =>{
+		res.status(200)
+		res.json(data.get({plain:true}))
+	}).catch(err =>{
+		res.status(500)
+		res.json({ error: err.message})
+	});
+});
 export default api;
