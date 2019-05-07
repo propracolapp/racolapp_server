0b0c9c2751a02f8c57f06e57768077c62ea188d7
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-05-07 12:32:37 +0200

    add route events

--- controllers/events.js
+++ controllers/events.js
@@ -1,12 +1,26 @@
 import { Router } from "express";
-import Users from '../models/Users';
+import Events from '../models/Events';
 
 const api = Router();
 
+// get events
 api.get("/", async (req, res) => {
+	const data = Events.findAll()
 	res.json({
-		Elements: "Users.findAll()"
-	});
+		data
+	}).status(200);
 });
 
+// get event by id
+api.get('/:id' async (req,res) =>{
+	
+	const data = await Event.findByPk(req.params.id)
+	res.json({
+		data
+	});
+})
+// Add user
+api.post('/', async (req, res) =>{
+
+})
 export default api;
