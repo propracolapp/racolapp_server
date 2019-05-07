bbeb4bcd72ce98bc7d0a24193f689944a28db3ad
Steeve Randriamampianina <st2diio@protonmail.ch>
2019-05-07 15:10:24 +0200

    Update events route

--- controllers/events.js
+++ controllers/events.js
@@ -61,4 +61,47 @@ api.post("/", async (req, res) => {
     });
 });
 
+// modify event by id
+api.put("/:id", (req, res) => {
+  Events.update(
+    {
+      name: req.body.name,
+      long: req.body.long,
+      lat: req.body.lat,
+      capacity: req.body.capacity,
+      date: req.body.date,
+      description: req.body.description,
+      duration: req.body.description,
+      counterViews: req.body.counterViews,
+      Users_ID: req.body.Users_ID,
+      TypeEvents_ID: req.body.TypeEvents_ID,
+      created_at: createdAt
+    },
+    { where: { ID: req.body.id }, returning: true, plain: true }
+  )
+	.then(function(data) {
+		res.status(200);
+		res.json(data.get({ plain: true}));
+	})
+	.catch(function(err){
+		res.status(500);
+		res.json({error: err.message });
+	})
+});
+
+//delete event by id
+api.delete("/:id", async (req, res) => {
+  await Events.destroy({
+    where: { ID: req.params.id }
+  })
+    .then(data => {
+      res.status(200);
+      res.json(data.get({ plain: true }));
+    })
+    .catch(err => {
+      res.status(500);
+      res.json({ error: err.message });
+    });
+});
+
 export default api;
