d0b6c79b949cc4f92a8d9b1b628c4755cec8b4f0
Steeve Randriamampianina <st2diio@protonmail.ch>
2019-05-07 09:09:03 +0200

    Add POST route

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,28 @@
-import {
-	Router
-} from "express";
+import { Router } from "express";
 import Users from "../models/Users";
 
 const api = Router();
 
-api.get("/", async (req,res) => {
+api.get("/", async (req, res) => {
+  const data = await Users.findOne({ where: { ID: 1 } });
+  console.log(data);
 
-	const data = await Users.findOne({where: {ID: 1}})
-	console.log(data);
-	
-	res.json({
-		route: "users",
-		user: data,
-		
-	});
+  res.json({
+    route: "users",
+    user: data
+  });
+});
+
+api.post("/", async (req, res) => {
+  const user = new Users(req.body);
+  user
+    .save()
+    .then(user => {
+      res.json("User added successfully");
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
 });
 
 export default api;
