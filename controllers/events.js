5e83dd9d73397ee4c3d9862319e08d6d89598c3b
Steeve Randriamampianina <st2diio@protonmail.ch>
2019-05-07 14:51:01 +0200

    fix update route Events

--- controllers/events.js
+++ controllers/events.js
@@ -31,33 +31,34 @@ api.get("/:id", async (req, res) => {
     });
 });
 // Add user
-api.post("/", async (req, res) => {});
-const createdAt = new now();
-Events.create(
-  {
-    name: req.body.name,
-    long: req.body.long,
-    lat: req.body.lat,
-    capacity: req.body.capacity,
-    date: req.body.date,
-    description: req.body.description,
-    duration: req.body.description,
-    counterViews: req.body.counterViews,
-    Users_ID: req.body.Users_ID,
-    TypeEvents_ID: req.body.TypeEvents_ID,
-    created_at: createdAt
-  },
-  { where: { ID: req.body.id }, returning: true, plain: true }
-)
-  .then(function(data) {
-    res.status(200);
-    res.json(data.get({ plain: true }));
-  })
-  .catch(function(err) {
-    res.status(500);
-    res.json({
-      error: err.message
+api.post("/", async (req, res) => {
+  const createdAt = new now();
+  Events.create(
+    {
+      name: req.body.name,
+      long: req.body.long,
+      lat: req.body.lat,
+      capacity: req.body.capacity,
+      date: req.body.date,
+      description: req.body.description,
+      duration: req.body.description,
+      counterViews: req.body.counterViews,
+      Users_ID: req.body.Users_ID,
+      TypeEvents_ID: req.body.TypeEvents_ID,
+      created_at: createdAt
+    },
+    { where: { ID: req.body.id }, returning: true, plain: true }
+  )
+    .then(function(data) {
+      res.status(200);
+      res.json(data.get({ plain: true }));
+    })
+    .catch(function(err) {
+      res.status(500);
+      res.json({
+        error: err.message
+      });
     });
-  });
+});
 
 export default api;
