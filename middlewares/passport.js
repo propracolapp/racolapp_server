ecc1cd5b4b96cbe617afebf2713e0a4f669ec709
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-05-10 12:34:55 +0200

    add passport

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -6,11 +6,38 @@ import Users from "../models/Users";
 passport.use(
 	new LocalStrategy(
 		{
-			usernameField: "nickname",
+			usernameField: "username",
 			passwordField: "password"
 		},
-		async (nickname, password, next) => {
-			const user= await Users.findOne({ where: { id }})
+		async (email, password, next) => {
+			const user = await Users.findOne({ where: { email } });
+			if (!user) {
+				return next("User is undefined");
+			}
+			if (!(await user.checkPassword(password))) {
+				return next("password incorrect");
+			}
+			return next(false, user);
+		}
+	)
+);
+
+passport.use(
+	new JwtStrategy(
+		{
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			secretOrKey: process.env.Token
+		},
+		async (jwtPayload, next) => {
+			try {
+				const user = await Users.findOne({ where: { id: jwtPayload.id } });
+				if (!user) {
+					return next("User doesn't exist");
+				}
+				return next(false, user);
+			} catch (err) {
+				return next(err.message);
+			}
 		}
 	)
 );
