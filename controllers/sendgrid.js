441222d21db6f4ea4fce3e47f19df8a3c829735a
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-05-10 11:17:04 +0200

    init sendgrid

--- controllers/sendgrid.js
+++ controllers/sendgrid.js
@@ -1,13 +1,34 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 import mail from "sendgrid";
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-	to: "test@example.com",
-	from: "test@example.com",
-	subject: "Sending with Twilio SendGrid is Fun",
-	text: "and easy to do anywhere, even with Node.js",
-	html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-};
-sgMail.send(msg);
+import { Router } from "express";
+
+const api = Router();
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+api.get("/registered", (req, res) => {
+	const msg = {
+		to: `${req.body.email}`,
+		from: "contact@racolapp.com",
+		subject: "Inscription success",
+		text: "...",
+		html: "<strong>Vous avez été bien inscript</strong>"
+	};
+	mail.send(msg);
+	if (res.status === 200) {
+		res
+			.json({
+				text: msg.text
+			})
+			.status(200).end();
+	} else {
+		res
+			.status(500)
+			.json({
+				error: "error"
+			})
+			.end();
+	}
+});
+
+export default api;
